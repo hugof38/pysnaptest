0aeed2dbe4a8041c33971343bae5593a4b281982
Hugo Filstroff <hugofilstroff@gmail.com>
2025-07-07 19:23:47 +0100

    Snapshot mock python

--- src/lib.rs
+++ src/lib.rs
@@ -8,20 +8,19 @@ use std::{env, path::Path};
 
 use csv::ReaderBuilder;
 
+use insta::assert_json_snapshot as assert_json_snapshot_macro;
 use insta::internals::{Redaction, SnapshotContents};
 use insta::Snapshot;
 use insta::{rounded_redaction, sorted_redaction};
 use once_cell::sync::Lazy;
-use pyo3::types::PyAnyMethods;
-use pyo3::FromPyObject;
+use pyo3::types::{PyAnyMethods, PyTuple};
 use pyo3::{
     exceptions::PyValueError,
     pyclass, pyfunction, pymethods, pymodule,
     types::{PyModule, PyModuleMethods},
     wrap_pyfunction, Bound, PyAny, PyErr, PyResult,
 };
-use serde::de::DeserializeOwned;
-use serde::Serialize;
+use pyo3::{FromPyObject, Py, PyObject, Python};
 
 const PYSNAPSHOT_SUFFIX: &str = "pysnap";
 
@@ -217,7 +216,9 @@ impl SnapshotInfo {
     }
 
     pub fn last_snapshot_path(&self, module_path: Option<String>) -> PyResult<PathBuf> {
-        let module_path = module_path.unwrap_or(module_path!().to_string()).replace("::", "__");
+        let module_path = module_path
+            .unwrap_or(module_path!().to_string())
+            .replace("::", "__");
         Ok(self.snapshot_folder.join(format!(
             "{module_path}__{}@pysnap.snap",
             self.last_snapshot_name()
@@ -225,7 +226,9 @@ impl SnapshotInfo {
     }
 
     pub fn next_snapshot_path(&self, module_path: Option<String>) -> PyResult<PathBuf> {
-        let module_path = module_path.unwrap_or(module_path!().to_string()).replace("::", "__");
+        let module_path = module_path
+            .unwrap_or(module_path!().to_string())
+            .replace("::", "__");
         Ok(self.snapshot_folder.join(format!(
             "{module_path}__{}@pysnap.snap",
             self.next_snapshot_name()
@@ -407,12 +410,12 @@ fn assert_snapshot(test_info: &SnapshotInfo, result: &Bound<'_, PyAny>) -> PyRes
     });
     Ok(())
 }
-/// Alternative macro that automatically detects argument count using count_args
+
 #[macro_export]
 macro_rules! create_snapshot_fn_auto {
-    ($f:expr, $name:expr $(, $arg:ident )* ; $serialize_input:expr) => {{
+    ($f:expr $(, $arg:ident )* ; serialize_macro = $serialize_macro:ident ; result_from_str=$result_from_str:expr) => {{
         let f = $f;
-        let name = $name;
+        let name = stringify!($f);
         let module_path = module_path!();
 
         move |$( $arg ),+, info: &SnapshotInfo, redactions: Option<HashMap<String, RedactionType>>, record: bool| -> Result<_, anyhow::Error> {
@@ -429,13 +432,15 @@ macro_rules! create_snapshot_fn_auto {
             }
 
             // Serialize the input using the passed closure
-            let input_tuple = ($( $arg ),+,);
-            ($serialize_input)(&settings, &snapshot_name, &input_tuple);
+            settings.bind(|| {
+                $serialize_macro!(format!("{snapshot_name}-request"), ($( $arg ),+));
+            });
+
 
             if record || !snapshot_path.exists() {
-                let result = f($( $arg ),+);
+                let result = f($( $arg ),+)?;
                 settings.bind(|| {
-                    insta::assert_json_snapshot!(snapshot_name, result);
+                    $serialize_macro!(snapshot_name, result);
                 });
                 Ok(result)
             } else {
@@ -444,7 +449,7 @@ macro_rules! create_snapshot_fn_auto {
                     .contents()
                 {
                     SnapshotContents::Text(content) => {
-                        Ok(serde_json::from_str(&content.to_string())?)
+                        Ok(($result_from_str)(content.to_string())?)
                     },
                     SnapshotContents::Binary(_) => Err(anyhow::anyhow!(
                         "Snapshot at {:?} is binary, which is not supported for deserialization",
@@ -455,117 +460,115 @@ macro_rules! create_snapshot_fn_auto {
         }
     }};
 
-    ($f:expr, $name:expr $(, $arg:ident )* ) => {
+    ($f:expr $(, $arg:ident )* ) => {
         create_snapshot_fn_auto!(
             $f,
-            $name,
             $( $arg ),+;
-            |settings: &insta::Settings, snapshot_name: &str, input: &_| {
-                settings.bind(|| {
-                    insta::assert_json_snapshot!(format!("{snapshot_name}-request"), input);
-                });
-            }
+            serialize_macro=assert_json_snapshot_macro;
+            result_from_str=|content: String| serde_json::from_str(&content)
         )
     };
 }
+macro_rules! assert_json_snapshot_depythonize {
+    ($snapshot_name:expr, ($( $arg:expr ),+) ) => {{
+        // Create a tuple of depythonized values
+        let input_tuple = (
+            $(
+                pythonize::depythonize::<serde_json::Value>($arg as &Bound<PyAny>).expect(&format!("Failed to depythonize {:?}", $arg))
+            ),+
+        );
 
-fn create_snapshot_fn<I, O, F>(f: F, name: String) -> impl Fn(&I, &SnapshotInfo, Option<HashMap<String, RedactionType>>, bool) -> Result<O, anyhow::Error>
-where
-    I: Serialize + std::fmt::Debug,
-    O: Serialize + DeserializeOwned,
-    F: Fn(&I) -> O + Clone,
-{
-    move |input: &I, info: &SnapshotInfo, redactions: Option<HashMap<String, RedactionType>>, record: bool| {
-        let finfo = SnapshotInfo {
-            snapshot_name: format!("{}_{}", info.snapshot_name, name),
-            ..(*info).clone()
-        };
-        
-        let snapshot_path = finfo.next_snapshot_path(Some(module_path!().to_string()))?;
-
-        let snapshot_name = finfo.snapshot_name();
-        let mut settings: insta::Settings = (&finfo).try_into()?;
-
-        for (selector, redaction) in redactions.unwrap_or_default() {
-            settings.add_redaction(selector.as_str(), redaction)
-        }
-
-        settings.bind(|| {
-            insta::assert_json_snapshot!(format!("{snapshot_name}-request"), input);
+        assert_json_snapshot_macro!($snapshot_name, input_tuple);
+    }};
+    ($snapshot_name:expr, $arg:expr) => {{
+        Python::with_gil(|py| {
+            let bound: &pyo3::Bound<PyAny> = $arg.bind(py);
+            let input_tuple = pythonize::depythonize::<serde_json::Value>(&bound).expect(&format!("Failed to depythonize {:?}", $arg));
+            assert_json_snapshot_macro!($snapshot_name, input_tuple);
         });
+    }};
+}
 
-        // Try to load existing snapshot via insta
-        if !snapshot_path.exists() || record {
-            // Compute the result
-            let result = f(input);
+#[pyclass]
+struct PyMockWrapper {
+    f: Box<
+        dyn for<'a> Fn(
+                &'a Bound<'_, PyTuple>,
+                &'a SnapshotInfo,
+                Option<HashMap<String, RedactionType>>,
+                bool,
+            ) -> Result<Py<PyAny>, anyhow::Error>
+            + Send
+            + Sync,
+    >,
+    snapshot_info: SnapshotInfo,
+    record: bool,
+}
 
-            settings.bind(|| {
-                insta::assert_json_snapshot!(snapshot_name, result);
-            });
-            Ok(result)
-        } else {
-            match Snapshot::from_file(&snapshot_path)
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?
-            .contents()
-            {
-                SnapshotContents::Text(content) => Ok(serde_json::from_str::<O>(&content.to_string())?),
-                SnapshotContents::Binary(_items) => Err(anyhow::anyhow!("Snapshot at {snapshot_path:?} is binary, which is not supported for deserialization"))                ,
-            }
-        }
+use std::clone::Clone;
+#[pymethods]
+impl PyMockWrapper {
+    fn __call__(&self, args: &Bound<'_, PyTuple>) -> PyResult<PyObject> {
+        (self.f)(args, &self.snapshot_info, None, self.record)
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))
     }
 }
 
-// fn create_snapshot_fn_py<'py, I, O>(f: Py<PyAny>, name: String) -> impl Fn(
-//     Python<'_>,
-//     &Bound<'_, PyTuple>,  // Bound args
-//     Option<&Bound<'_, PyDict>>,  // Bound kwargs (now correct type)
-//     &SnapshotInfo, Option<HashMap<String, RedactionType>>, bool) -> PyResult<Bound<'py, PyAny>>
-// {
-//     move |py, args, kwargs, info, redactions, record| {
-//         let finfo = SnapshotInfo {
-//             snapshot_name: format!("{}_{}", info.snapshot_name, name),
-//             ..(*info).clone()
-//         };
-        
-//         let snapshot_path = finfo.next_snapshot_path()?;
+fn wrap_py_fn_snapshot(
+    py_fn: PyObject,
+) -> impl for<'b> Fn(
+    &'b Bound<'_, PyTuple>,
+    &'b SnapshotInfo,
+    Option<HashMap<String, RedactionType>>,
+    bool,
+) -> Result<Py<PyAny>, anyhow::Error>
+       + Send
+       + Sync {
+    move |args: &Bound<'_, PyTuple>,
+          info: &SnapshotInfo,
+          redactions: Option<HashMap<String, RedactionType>>,
+          record: bool| {
+        let py_fn_cloned = Python::with_gil(|py| py_fn.clone_ref(py));
 
-//         let snapshot_name = finfo.snapshot_name();
-//         let mut settings: insta::Settings = (&finfo).try_into()?;
+        let call_fn = move |args: &Bound<'_, PyTuple>| -> PyResult<PyObject> {
+            Python::with_gil(|py| py_fn_cloned.call1(py, args))
+        };
 
-//         for (selector, redaction) in redactions.unwrap_or_default() {
-//             settings.add_redaction(selector.as_str(), redaction)
-//         }
+        let wrapped_fn = create_snapshot_fn_auto!(
+            call_fn, args;
+            serialize_macro=assert_json_snapshot_depythonize;
+            result_from_str=|content: String| -> PyResult<PyObject> {
+                Python::with_gil(|py| {
+                    let value: serde_json::Value = serde_json::from_str(&content)
+                        .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+                    let obj = pythonize::pythonize(py, &value)?;
+                    Ok(obj.into())
+                })
+            }
+        );
 
-//         settings.bind(|| {
-//             insta::assert_json_snapshot!(format!("{snapshot_name}-request"), input);
-//         });
+        wrapped_fn(args, info, redactions, record)
+    }
+}
 
-//         // Try to load existing snapshot via insta
-//         if !snapshot_path.exists() || record {
-//             // Compute the result
-//             let result = f.call(py, args, kwargs)?.bind(py);
-
-//             settings.bind(|| {
-//                 insta::assert_json_snapshot!(snapshot_name, pythonize::depythonize(&result).unwrap());
-//             });
-//             Ok(*result)
-//         } else {
-//             match Snapshot::from_file(&snapshot_path)
-//             .map_err(|e| anyhow::anyhow!(e.to_string()))?
-//             .contents()
-//             {
-//                 SnapshotContents::Text(content) => {
-//                     let value: serde_json::Value = serde_json::from_str(&content.to_string())?;
-//                     let py_obj = pythonize::pythonize(py, &value)?;
-//                     Ok(py_obj)
-//                 },
-//                 SnapshotContents::Binary(_items) => Err(anyhow::anyhow!("Snapshot at {snapshot_path:?} is binary, which is not supported for deserialization"))                ,
-//             }
-//         }
-//     }
-// }
-
-
+#[pyfunction]
+fn create_mocked_pyfn(
+    py_fn: PyObject,
+    snapshot_info: SnapshotInfo,
+    record: bool,
+) -> PyResult<PyObject> {
+    Python::with_gil(|py| {
+        let callable = Py::new(
+            py,
+            PyMockWrapper {
+                f: Box::new(wrap_py_fn_snapshot(py_fn)),
+                snapshot_info,
+                record,
+            },
+        )?;
+        Ok(callable.into())
+    })
+}
 
 #[pymodule]
 #[pyo3(name = "_pysnaptest")]
@@ -576,6 +579,7 @@ fn pysnaptest(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(assert_binary_snapshot, m)?)?;
     m.add_function(wrap_pyfunction!(assert_json_snapshot, m)?)?;
     m.add_function(wrap_pyfunction!(assert_csv_snapshot, m)?)?;
+    m.add_function(wrap_pyfunction!(create_mocked_pyfn, m)?)?;
     m.add_class::<PySnapshot>()?;
     Ok(())
 }
@@ -583,7 +587,11 @@ fn pysnaptest(m: &Bound<'_, PyModule>) -> PyResult<()> {
 #[cfg(test)]
 mod tests {
 
-    use std::{cell::Cell, path::{Path, PathBuf}, rc::Rc};
+    use std::{
+        cell::Cell, ffi::CString, path::{Path, PathBuf}, rc::Rc
+    };
+
+    use pyo3::{types::PyDict, IntoPyObject};
 
     use super::*;
 
@@ -636,16 +644,16 @@ mod tests {
     #[test]
     fn test_snapshot_json_or_mock_creates_and_reads_snapshot() -> Result<(), anyhow::Error> {
         let input_1 = 4;
-    
+
         // Shared counter to track how many times the function is called
         let call_count = Rc::new(Cell::new(0));
         let call_count_clone = Rc::clone(&call_count);
-    
+
         let f = |i| {
             call_count_clone.set(call_count_clone.get() + 1);
-            i * 2 
+            Ok::<_, anyhow::Error>(i * 2)
         };
-        
+
         let snaphot_folder_path = snapshot_folder_path();
         let snapshot_info = SnapshotInfo {
             snapshot_folder: snaphot_folder_path,
@@ -653,19 +661,73 @@ mod tests {
             relative_test_file_path: None,
             allow_duplicates: true,
         };
-    
-        let snapshot_json_or_mock = create_snapshot_fn_auto!(f, "snapshot_json_or_mock".to_string(), x);
-    
+
+        let snapshot_json_or_mock = create_snapshot_fn_auto!(f, x);
+
         // First run: record mode, should call the function
         let result_1: i32 = snapshot_json_or_mock(input_1, &snapshot_info, None, true)?;
         assert_eq!(result_1, 8);
-        assert_eq!(call_count.get(), 1, "Function should have been called once during recording");
-    
+        assert_eq!(
+            call_count.get(),
+            1,
+            "Function should have been called once during recording"
+        );
+
         // Second run: replay mode, should NOT call the function
         let result_2: i32 = snapshot_json_or_mock(input_1, &snapshot_info, None, false)?;
         assert_eq!(result_2, 8);
-        assert_eq!(call_count.get(), 1, "Function should NOT have been called again during replay");
-    
+        assert_eq!(
+            call_count.get(),
+            1,
+            "Function should NOT have been called again during replay"
+        );
+
         Ok(())
     }
+
+    #[test]
+    fn test_create_mocked_pyfn_creates_and_reads_snapshot() -> Result<(), anyhow::Error> {
+        pyo3::prepare_freethreaded_python();
+        let snapshot_info = SnapshotInfo {
+            snapshot_name: "test_create_mocked_pyfn".to_string(),
+            relative_test_file_path: None,
+            allow_duplicates: true,
+            snapshot_folder: snapshot_folder_path(),
+        };
+    
+        Python::with_gil(|py| -> PyResult<()> {
+            // Define a Python function with a mutable counter
+            let code = r#"
+counter = {"calls": 0}
+def compute(x):
+    counter["calls"] += 1
+    return {"result": x * 10, "calls": counter["calls"]}
+"#;
+    
+            let module = PyModule::from_code(py, CString::new(code)?.as_c_str(), CString::new("testmod.py")?.as_c_str(), CString::new("testmod")?.as_c_str())?;
+            let py_fn: Py<PyAny> = module.getattr("compute")?.into_pyobject(py)?.into();
+    
+            // Wrap with snapshot function in RECORDING mode
+            let wrapper_obj = create_mocked_pyfn(py_fn.clone_ref(py), snapshot_info.clone(), true)?;
+            let wrapper = wrapper_obj.bind(py);
+    
+            let args = PyTuple::new(py, 7.into_pyobject(py))?;
+    
+            let result1: Bound<'_, PyDict> = wrapper.call1((args,))?.extract()?;
+            assert_eq!(result1.get_item("result").unwrap().extract::<i32>()?, 70);
+            assert_eq!(result1.get_item("calls").unwrap().extract::<i32>()?, 1);
+    
+            let wrapper_obj = create_mocked_pyfn(py_fn, snapshot_info.clone(), false)?;
+            let wrapper = wrapper_obj.bind(py);
+            let args = PyTuple::new(py, 7.into_pyobject(py))?;
+    
+            let result2: Bound<'_, PyDict> = wrapper.call1((args,))?.extract()?;
+            assert_eq!(result2.get_item("result").unwrap().extract::<i32>()?, 70);
+            assert_eq!(result2.get_item("calls").unwrap().extract::<i32>()?, 1);
+    
+            Ok(())
+        })?;
+    
+        Ok(())
+    }    
 }
