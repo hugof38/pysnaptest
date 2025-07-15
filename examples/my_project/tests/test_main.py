7f05dbd0d9888a27f1bea6ead4e76c981a7fc479
Hugo Filstroff <hugofilstroff@gmail.com>
2025-07-15 20:08:27 +0100

    Ruff format

--- examples/my_project/tests/test_main.py
+++ examples/my_project/tests/test_main.py
@@ -2,12 +2,15 @@ from pysnaptest import snapshot, mock_json_snapshot
 from my_project.main import main, use_http_request, http_request
 from unittest.mock import patch
 
+
 @snapshot()
 def test_main():
     return main()
 
+
 @snapshot()
 def test_use_http_request():
-    with patch("my_project.main.http_request", side_effect=mock_json_snapshot(http_request)):
+    with patch(
+        "my_project.main.http_request", side_effect=mock_json_snapshot(http_request)
+    ):
         return use_http_request()
-
