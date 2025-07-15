fa2dc88a403e92d85f3fd9ea2aeca2056eacadf3
Hugo Filstroff <hugofilstroff@gmail.com>
2025-07-15 20:04:02 +0100

    Add example for http mock

--- examples/my_project/my_project/main.py
+++ examples/my_project/my_project/main.py
@@ -1,2 +1,15 @@
+import requests
+from typing import Any
+
+def http_request(url: str) -> dict[str, Any]:
+    print(f"Making request to url {url}")
+    resp = requests.get(url)
+    assert resp.status_code == 200
+    return resp.json()
+
+def use_http_request() -> int:
+    resp = http_request("https://jsonplaceholder.typicode.com/todos/1")
+    return resp["id"] + 1
+
 def main():
     return {"main": "result"}
