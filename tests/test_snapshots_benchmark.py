770cb1122c855972ce5c9a7a42a441c23ab35cf0
Hugo Filstroff <hugofilstroff@gmail.com>
2025-07-27 00:09:58 +0100

    No benchmark on CI

--- tests/test_snapshots_benchmark.py
+++ tests/test_snapshots_benchmark.py
@@ -15,12 +15,14 @@ def generate_json(n: int) -> dict:
     return {str(i): [i] * i for i in range(n)}
 
 
+@pytest.mark.benchmark
 @pytest.mark.parametrize("size", [10, 1000])
 def test_pysnaptest_benchmark(tmp_path, benchmark, size):
     data = generate_json(size)
     benchmark(lambda: assert_json_snapshot(data, allow_duplicates=True))
 
 
+@pytest.mark.benchmark
 @pytest.mark.parametrize("size", [10, 1000])
 def test_syrupy_benchmark(snapshot: SyrupSnapshot, benchmark, size):
     data = generate_json(size)
